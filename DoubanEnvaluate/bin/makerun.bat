87adac02d8d13bb3acf8f285a57e6990173f2c16
KMPalgorithm <zhaotianyuan2@126.com>
2019-11-28 18:27:11 +0800

    sort the Dir

---
+++ DoubanEnvaluate/bin/makerun.bat
@@ -0,0 +1,2 @@
+javac Test.java -classpath ".;D:\Project_Indeed\Database\DatabaseDesign\DatabaseDesign\DoubanEnvaluate\src\ExtJRE\*" Test.java
+java -classpath ".;D:\Project_Indeed\Database\DatabaseDesign\DatabaseDesign\DoubanEnvaluate\src\ExtJRE\*" Test
